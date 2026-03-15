642707c3f03c8944eb32835eaf9c06fc7e8c2ef1
syedbarimanjan <syedbarimanjan@gmail.com>
2026-03-15 07:16:42 +0500

    made error handling better and let user pass argument

--- week-1/src/main.rs
+++ week-1/src/main.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use std::env;
 
 #[derive(Deserialize)]
 struct Story {
@@ -10,7 +11,7 @@ struct Story {
 }
 
 
-fn api_request() -> Result<Story, Box<dyn std::error::Error>> {
+fn api_request() -> Result<Vec<Story>, Box<dyn std::error::Error>> {
     let client = reqwest::blocking::Client::new();
     
     let top_ids: Vec<u64> = client.get("https://hacker-news.firebaseio.com/v0/topstories.json")
@@ -18,38 +19,44 @@ fn api_request() -> Result<Story, Box<dyn std::error::Error>> {
     // .expect("Failed to fetch top stories")
     .json()?;
     // .expect("Failed to parse stroy IDs");
+
+    let args:Vec<String> = env::args().collect();
     
-    for (i, id) in top_ids.iter().take(10).enumerate() {
+    
+    let mut stories: Vec<Story> = Vec::with_capacity(args[1].parse().unwrap_or(10));
+    
+    for (i, id) in top_ids.iter().take(args[1].parse().unwrap_or(10)).enumerate() {
         let url = format!("https://hacker-news.firebaseio.com/v0/item/{id}.json");
-    
+        
         let story: Story = client
         .get(&url)
         .send()?
         // .expect("failed to fetch stroy")
         .json()?;
-        // .expect("failed to parse story");
+    // .expect("failed to parse story");
     
         let link = story.url.as_deref().unwrap_or("(no URL)");
         println!("{}. {} ({} points by {}) ({} descendants)", i+1, story.title, story.score, story.by, story.descendants);
         println!("   {}\n", link);
         
-        // Ok(story);
+        stories.push(story);
     }
-    Err("no stores found".into())
+    let _ = Err::<Vec<Story>, Box<dyn std::error::Error>>("beep boop no data found".into());
+    Ok(stories)
 }
 
 fn main(){
-    let api_result: Result<Story, Box<dyn std::error::Error>> = api_request();
+    let api_result: Result<Vec<Story>, Box<dyn std::error::Error>> = api_request();
 
     match api_result {
         Ok(response) => {
-            println!("Data recieved: {}", response.title);
+            println!("Data recieved: {}", response[0].title);
             // let link = response.url.as_deref().unwrap_or("(no URL)");
             // println!("{}. {} ({} points by {})", i+1, response.title, response.score, response.by);
             // println!("   {}\n", link);
         }
         Err(error) => {
-            println!("Error{:?}", error);
+            println!("Error {:?}", error);
         }
     }
 }
